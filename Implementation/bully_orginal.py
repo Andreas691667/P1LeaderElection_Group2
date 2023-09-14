a5c984ca1f77a3efef79326b1638279a474ae0a3
Andreas691667 <andreaskaag@hotmail.com>
2023-09-14 14:02:56 +0200

    Co-authored-by: BusterSalomon <BusterSalomon@users.noreply.github.com>

--- Implementation/bully_orginal.py
+++ Implementation/bully_orginal.py
@@ -2,6 +2,7 @@ from enum import Enum
 from dataclasses import dataclass
 from threading import Event, Thread
 from queue import Empty, Queue
+import time
 
 
 @dataclass
@@ -10,6 +11,7 @@ class ProcessStates(Enum):
     ALIVE = 1
     DEAD = 2
 
+
 @dataclass
 class MessageTypes(Enum):
     """types of messages that can be sent"""
@@ -17,57 +19,63 @@ class MessageTypes(Enum):
     OK = 2
     COORDINATOR = 3
 
+
 class Process:
     """Processes in the system"""
 
     def __init__(self, _id):
         self.message_thread = Thread(target=self.message_handler, daemon=True)
         self.stop_worker = Event()
-        self.message_queue = Queue() # tuple[sender_id, type]
-        
+        self.message_queue = Queue()  # tuple[sender_id, type]
+
         self._id = _id
         self.state = ProcessStates.ALIVE
         self.processes = []
         self.oks = 0
-        
+        self.coordinater = False
+
     def start_thread(self):
         """Start the message handler thread"""
         self.message_thread.start()
 
     def kill(self):
         """Kill the process"""
+        self.coordinater = False
         self.state = ProcessStates.DEAD
-    
+
     def get_id(self):
         return self._id
-    
+
     def get_process(self, _id):
         return self.processes[_id]
-    
+
     def enqueue_message(self, sender_id, msg_type):
-        self.message_queue.put((sender_id, msg_type))        
-    
+        self.message_queue.put((sender_id, msg_type))
+
     def message_handler(self):
         """"Receive message from another process"""
-        #worker method for thread
+        # worker method for thread
         while not self.stop_worker.is_set():
             try:
                 process_id, msg_type = self.message_queue.get(timeout=1)
             except Empty:
                 pass
             else:
-                match msg_type:                     
+                match msg_type:
                     case MessageTypes.ELECTION:
-                        print(f"{self.get_id} received election from {process_id}")
-                        process = self.get_process(process_id)
-                        process.enqueue_message(self.get_id(), MessageTypes.OK)
-                        self.start_election()
+                        print(f"{self._id} received election from {process_id} \n")
+                        if not self.state == ProcessStates.DEAD:
+                            process = self.get_process(process_id)
+                            process.enqueue_message(self._id, MessageTypes.OK)
+                            self.start_election()
 
                     case MessageTypes.OK:
-                        print(f"{self.get_id} received OK from {process_id}")
-                        
+                        self.oks = self.oks + 1
+                        print(f"{self._id} received OK from {process_id}")
+
                     case MessageTypes.COORDINATOR:
-                        print(f"{self.get_id} received coordinator from {process_id}")
+                        print(
+                            f"{self._id} received coordinator from {process_id}")
 
                     case _:
                         pass
@@ -75,37 +83,51 @@ class Process:
     def send_coordinator(self):
         """Send coordinator message to all processes"""
         for process in self.processes:
-            process.enqueue_message(self.get_id(), MessageTypes.COORDINATOR)
+            process.enqueue_message(self._id, MessageTypes.COORDINATOR)
 
-    # Starts an elec
-    def start_election (self):
-        # Send election msg to processes with higher id's
+    # Starts an election
+    def start_election(self):
+        """Send election msg to processes with higher id's"""
         for process in self.processes:
-            if process.get_id() > self.get_id():
-                process.enqueue_message(self.get_id, MessageTypes.ELECTION)
-        
-        # check for OKs
-        # if no OKs, send coordinator msg to all processes
-        if self.oks == 0:
-            self.send_coordinator()
-      
-if __name__ == "__main__":
-    #create 5 processes as threads and start them
-    processes = []
-    for i in range(5):
-        processes.append(Process(i))
+            if process.get_id() > self._id:
+                print(f"{self._id} sending election to {process.get_id()}")
+                process.enqueue_message(self._id, MessageTypes.ELECTION)
 
-    # inform each process of all the other processes by adding them to the process list
-    for i in range(5):
-        processes[i].processes = processes
-        
-    
-    for p in processes:
+        # Change state to waitForOks
+
+        # check for oks in a while loop for a maximum of 5 seconds
+        # if no oks, send coordinator msg to all processes
+        start = time.time()
+        while self.oks == 0:
+            end = time.time()
+            if end - start > 5:
+                self.coordinater = True
+                self.send_coordinator()
+                break
+
+        # if we received any oks, do nothing. somebody else took over
+        # print(f"{self._id} received no oks")
+
+
+if __name__ == "__main__":
+    # create 5 processes and put them in a list
+    N = 3
+    all_processes = []
+    for i in range(N):
+        all_processes.append(Process(i))
+
+    # inform each process of all the other all_processes by adding them to the process list and remove self from list
+    # pass them as references to each process
+    for i in range(N):
+        all_processes[i].processes = all_processes
+
+    # start all all_processes
+    for p in all_processes:
         p.start_thread()
-    
+
     # simulate a process dying and sending an election message
-    processes[2].kill()
-    processes[2].enqueue_message(processes[2].get_id(), MessageTypes.ELECTION)
+    all_processes[2].kill()           # process 1 dies
+    all_processes[0].start_election() # process 2 starts election
 
     while True:
         pass
