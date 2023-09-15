b2fcfe6895831a10544c95b01fa4c96369c564c7
Andreas691667 <andreaskaag@hotmail.com>
2023-09-15 13:56:43 +0200

    inserted comments

--- Implementation/bully_orginal.py
+++ Implementation/bully_orginal.py
@@ -1,8 +1,8 @@
 # from enum import Enum
 # from dataclasses import dataclass
 from threading import Event, Thread
-from queue import Empty, Queue
-# import time
+from queue import Empty, PriorityQueue
+import time
 
 # Time interval for becoming coordinator
 TIMEOUT = 2
@@ -16,9 +16,9 @@ DEAD = 4
 
 
 # Message types
-ELECTION = 1
-OK = 2
-COORDINATOR = 3
+ELECTION = 2
+OK = 3
+COORDINATOR = 1
 
 
 class Process:
@@ -26,22 +26,23 @@ class Process:
 
     def __init__(self, _id):
         self.message_thread = Thread(target=self.state_machine, daemon=True)
-
         self.stop_worker = Event()
-        self.message_queue = Queue()  # tuple[sender_id, type]
+        self.message_queue = PriorityQueue()  # tuple[sender_id, type]
         self._id = _id
-        self.state = ALIVE
+        self.state = ALIVE  # initial state
         self.processes = []
         self.oks = 0
         self.coordinator_msg_sent = False
+        self.msg_count = 0          # number of messages sent, metric for performance
 
     def start_thread(self):
         """Start the message handler thread"""
         self.message_thread.start()
 
     def kill(self):
-        """Kill the process"""
+        """Kill the process by setting state and stopiing worker thread"""
         self.state = DEAD
+        self.stop_worker.set()
         print(f"{self._id} is dead \n")
 
     def get_id(self):
@@ -54,37 +55,47 @@ class Process:
 
     def enqueue_message(self, sender_id, msg_type):
         """Enqueue message to be processed by the state machine"""
-        self.message_queue.put((sender_id, msg_type))
+        self.msg_count += 1
+        self.message_queue.put((msg_type, sender_id))
 
     def message_handler(self, process_id, msg_type):
-        """"Receive message from another process"""
+        """"Handle message from another process"""
+        # respond to election message by sending OK
         if msg_type == ELECTION:
-            if not self.state == DEAD:
-                print(f"{self._id} received election from {process_id} \n")
-                process = self.get_process(process_id)
-                process.enqueue_message(self._id, OK)
-                print(f"{self._id} sent OK to {process_id} \n")
-                self.start_election()
-        elif msg_type == OK:
+            print(f"{self._id} received election from {process_id} \n")
+            process = self.get_process(process_id)
+            process.enqueue_message(self._id, OK)
+            print(f"{self._id} sent OK to {process_id} \n")
+            self.start_election()
+        
+        # respond to OK message by incrementing OK count and changing state (TODO: Should maybe not change here...)
+        elif msg_type == OK and self.state == WAITING_FOR_OK:
             self.oks += 1
             print(
                 f"{self._id} received OK from {process_id} and current oks is {self.oks} \n")
+            self.state = ALIVE
 
+        # accept coordinator message and do nothing
         elif msg_type == COORDINATOR:
             print(f"{self._id} received coordinator from {process_id} \n")
 
     def state_machine(self):
-        """State machine for process"""
+        """State machine for process. Worker method"""
+        # try to get message from queue, if empty, check state and do something
         while not self.stop_worker.is_set():
             try:
-                process_id, msg_type = self.message_queue.get(timeout=1)
+                msg_type, process_id = self.message_queue.get(timeout=1)
 
             except Empty:
-                if self.state == ALIVE or self.state == DEAD:
+                # if state is ALIVE, do nothing
+                if self.state == ALIVE:
                     pass
+                # if state is COORDINATOR, send coordinator message to all processes if not already sent
                 elif self.state == COORDINATOR:
                     if not self.coordinator_msg_sent:
                         self.send_coordinator()
+                # if state is WAITING_FOR_OK, check if OK count is > 0, if so, change state to ALIVE, else send coordinator message
+                # TODO: Maybe this is superfluous, since we change state to ALIVE when we receive OK message but this is not ideal
                 elif self.state == WAITING_FOR_OK:
                     if self.oks > 0:
                         self.oks = 0
@@ -92,9 +103,11 @@ class Process:
                     else:
                         self.send_coordinator()
 
+                # if state is ELECTING, start election
                 elif self.state == ELECTING:
                     self.start_election()
 
+            # if message queue is not empty, handle message
             else:
                 self.message_handler(process_id, msg_type)
 
@@ -139,9 +152,28 @@ if __name__ == "__main__":
     for p in all_processes:
         p.start_thread()
 
-    # simulate a process dying and sending an election message
-    # all_processes[4].kill()           # process 1 dies
-    all_processes[0].start_election()  # process 2 starts election
+    # start election at highest priority process
+    all_processes[N-1].start_election()
+    # wait for convergence
+    time.sleep(2)
+    # simulate process failure and new election
+    all_processes[4].kill()           # process 2 dies
+    time.sleep(2)
+    all_processes[0].start_election()  # process 1 starts election
 
+    # wait for 5 seconds and then stop all all_processes
+    start = time.time()
     while True:
-        pass
+        time.sleep(.1)
+        if time.time() - start > 5:
+            break
+
+    msg_count = 0
+    for p in all_processes:
+        msg_count += p.msg_count
+        p.stop_worker.set()
+
+    print(f"Total messages sent: {msg_count}")
+
+    # while True:
+    #     pass
---
+++ Implementation/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+    # all_processes[3].start_election()  # process 2 starts election
+    # time.sleep(5)
