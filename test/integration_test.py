160d523956b79ca40b92fda6fd1909ee2b528870
Andreas691667 <andreaskaag@hotmail.com>
2023-09-28 12:21:43 +0200

    newest tests

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,16 +1,18 @@
 import sys
 sys.path.insert(0, "./src")
-from bully_improved import ProcessImproved
-from bully_orginal import ProcessOriginal
-import unittest
-from time import sleep
 from types_ import *
+from time import sleep
+import unittest
+from bully_orginal import ProcessOriginal
+from bully_improved import ProcessImproved
+
+
 
 class SystemTestsOriginal(unittest.TestCase):
     """Testing simulations of the bully algorithm"""
 
     def setUp(self) -> None:
-        self.N = 10
+        self.N = 30 
         self.all_processes = []
         for i in range(self.N):
             self.all_processes.append(ProcessOriginal(i))
@@ -46,12 +48,13 @@ class SystemTestsOriginal(unittest.TestCase):
         # check that all all_processes have the correct coordinator
         for i in range(self.N-1):
             self.assertEqual(self.all_processes[i].coordinator, self.N-1)
-    
+
 
 class SystemTestsImproved(unittest.TestCase):
     """Testing simulations of the improved bully algorithm"""
+
     def setUp(self) -> None:
-        self.N = 10
+        self.N = 30 
         self.all_processes = []
         for i in range(self.N):
             self.all_processes.append(ProcessImproved(i))
@@ -86,7 +89,9 @@ class SystemTestsImproved(unittest.TestCase):
 
         # check that all all_processes have the correct coordinator
         for i in range(self.N-1):
-            self.assertEqual(self.all_processes[i].current_coordinator, self.N-1)
+            self.assertEqual(
+                self.all_processes[i].current_coordinator, self.N-1)
+
 
 # system test for 'bully_improved.py'
 if __name__ == "__main__":
