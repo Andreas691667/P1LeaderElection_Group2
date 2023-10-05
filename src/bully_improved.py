55e5f4c29b6fee8475c25cdda6a4841b951d9d0c
BusterSalomon <54437784+BusterSalomon@users.noreply.github.com>
2023-10-05 11:01:28 +0200

    fixed mistake

--- src/bully_improved.py
+++ src/bully_improved.py
@@ -68,7 +68,7 @@ class ProcessImproved:
             self.election_in_progess = False
 
         elif msg_type == YOU_ARE_COORDINATOR:
-            self.current_coordinator = self._id
+            # self.current_coordinator = self._id
             self.start_election()
 
     def state_machine(self):
--- src/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    # all_processes[3].start_election()  # process 2 starts election
-    # time.sleep(5)
